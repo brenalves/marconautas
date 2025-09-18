09015d43e9efd24cb9c7bcfd8a967c7e8edc2382
Breno Alves <brenosoares4303@gmail.com>
2025-09-18 19:50:20 -0300

    basic setup

---
+++ run.sh
@@ -0,0 +1,19 @@
+sudo apt install pkg-config libwayland-dev libxrandr-dev libxkbcommon-dev libxinerama-dev libxcursor-dev
+
+git submodule init
+git submodule update
+
+cd deps/paho.mqtt.c
+cmake -B build -DPAHO_BUILD_SHARED=OFF -DPAHO_BUILD_STATIC=ON
+cd build
+sudo make install
+
+cd ../../glfw
+cmake -B build 
+cd build
+sudo make install
+
+cd ../../../
+cmake -B build
+cd build
+sudo make
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,73 @@
 #include <iostream>
 #include <MQTTClient.h>
+#include <string>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+bool isLogged = false;
+std::string
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
 
-    MQTTClient client;
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW, aborting..." << std::endl;
+        return 1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Marconautas", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create window, aborting..." << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // IF using Docking Branch
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        if(!isLogged)
+        {
+        
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
 
     return 0;
 }
