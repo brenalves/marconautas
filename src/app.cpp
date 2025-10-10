4eab234827ddc3e767ab802a766aa603adfc9ea4
Breno Soares <brenosoares4303@gmail.com>
2025-10-10 13:15:26 -0300

    ops, i messed with client destructor

--- src/app.cpp
+++ src/app.cpp
@@ -24,19 +24,22 @@ App::~App()
     // Cleanup
     _instance = nullptr;
 
-    // Publish offline status if client is connected
-    std::string statusTopic = "dev/marconautas/user/status/" + _client->getClientId();
-
-    Message statusMessage;
-    statusMessage.type = MessageType::STATUS;
-    statusMessage.sender = _client->getClientId();
-    statusMessage.content = "offline";
-    statusMessage.timestamp = std::chrono::system_clock::now();
-
-    _client->publish(statusTopic.c_str(), statusMessage, true);
-
     if (_client)
+    {
+        // Publish offline status if client is connected
+        std::string statusTopic = "dev/marconautas/user/status/" + _client->getClientId();
+
+        Message statusMessage;
+        statusMessage.type = MessageType::STATUS;
+        statusMessage.sender = _client->getClientId();
+        statusMessage.content = "offline";
+        statusMessage.timestamp = std::chrono::system_clock::now();
+
+        _client->publish(statusTopic.c_str(), statusMessage, true);
+        
+        _client->disconnect();
         delete _client;
+    }
 
     if (_window)
         delete _window;
@@ -76,7 +79,7 @@ void App::onLoginButtonSubmit(const char *username)
         _client->setOnStatusMessageCallback(std::bind(&App::onStatusMessage, this, std::placeholders::_1));
         _client->connect();
         _client->subscribe("dev/marconautas/user/status/+"); // Subscribe to user status updates
-        _client->subscribe("dev/marconautas/marcohub");            // Subscribe to global chat
+        _client->subscribe("dev/marconautas/marcohub");      // Subscribe to global chat
 
         // Publish online status
         std::string statusTopic = "dev/marconautas/user/status/" + std::string(username);
@@ -102,10 +105,10 @@ void App::onLoginButtonSubmit(const char *username)
 
 void App::onStatusMessage(Message *message)
 {
-    if(message->content == "online")
+    if (message->content == "online")
     {
         // Add user to active users list if not already present
-        if(std::find(_activeUsers.begin(), _activeUsers.end(), message->sender) == _activeUsers.end())
+        if (std::find(_activeUsers.begin(), _activeUsers.end(), message->sender) == _activeUsers.end())
         {
             _activeUsers.push_back(message->sender);
         }
@@ -114,7 +117,7 @@ void App::onStatusMessage(Message *message)
     {
         // Remove user from active users list
         auto it = std::find(_activeUsers.begin(), _activeUsers.end(), message->sender);
-        if(it != _activeUsers.end())
+        if (it != _activeUsers.end())
         {
             _activeUsers.erase(it);
         }
